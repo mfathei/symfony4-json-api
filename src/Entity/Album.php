4bbed304bb4f42bf750d16c8da2ec48ba7923a9a
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-08 17:34:35 +0200

    create Album and AlbumType form to validate our request data

---
+++ src/Entity/Album.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AlbumRepository")
+ */
+class Album
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $releaseDate;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $trackCount;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Album
+     */
+    public function setTitle($title): Album
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getReleaseDate(): ?\DateTime
+    {
+        return $this->releaseDate;
+    }
+
+    /**
+     * @param \DateTime $releaseDate
+     * @return Album
+     */
+    public function setReleaseDate($releaseDate): Album
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getTrackCount(): ?int
+    {
+        return $this->trackCount;
+    }
+
+    /**
+     * @param int $trackCount
+     * @return Album
+     */
+    public function setTrackCount($trackCount): Album
+    {
+        $this->trackCount = $trackCount;
+
+        return $this;
+    }
+}
